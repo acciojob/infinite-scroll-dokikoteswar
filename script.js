e8769aa4f0682d824b0f6a33736cc1120c41e0e9
dokikoteswar <139772409+dokikoteswar@users.noreply.github.com>
2023-09-26 15:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const scrollContainer = document.getElementById("infi-list");
+let i = 0;
+function addOneItem() {
+  const li = document.createElement("li");
+  li.innerText = `Item ${++i}`;
+  scrollContainer.appendChild(li);
+}
 
+for (let i = 0; i <= 10; i++) {
+  addOneItem();
+}
+scrollContainer.addEventListener("scroll", () => {
+  const maxScrollHeight =
+    scrollContainer.scrollHeight - scrollContainer.clientHeight;
+  let currentScrollTop = scrollContainer.scrollTop;
+
+  let threshold = Math.abs(maxScrollHeight - currentScrollTop);
+
+  if (threshold <= 5) {
+    for (let i = 0; i < 2; i++) {
+      addOneItem();
+    }
+  }
+});
